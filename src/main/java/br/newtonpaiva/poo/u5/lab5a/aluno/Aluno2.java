f54b6e62393d5b298eafb179ae5d2239023f594c
w.pires.ext.planerio <w.pires.ext@planerio.de>
2021-06-26 17:34:49 -0300

    Lab5a: fix

--- src/main/java/br/newtonpaiva/poo/u5/lab5a/aluno/Aluno2.java
+++ src/main/java/br/newtonpaiva/poo/u5/lab5a/aluno/Aluno2.java
@@ -14,7 +14,7 @@ public class Aluno2 {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
     
     private String matricula;
     
@@ -36,11 +36,11 @@ public class Aluno2 {
 		this.dataDeNascimento = dataDeNascimento;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
